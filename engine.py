65d504893d258dc5a0c2c63d89b6fc0c53128e66
Eric Moyer <ejmoyer3@comcast.net>
2020-02-03 20:44:19 -0500

    Added more choices such as OBSERVE, SAVE (wip), CHANGE ROOM and HELP.

--- engine.py
+++ engine.py
@@ -11,29 +11,63 @@ class Map(object):
 
 class Engine(object):
 
-    def __init__(self, test):
-        self.test = test
+    def __init__(self, map):
+        self.map = map
 
     def run(self):
-        self.test = RoomOne()
-        self.test.desc()
+        self.map = RoomOne()
+        print("Welcome to my game! Say 'OBSERVE' to get your bearings!")
+        current_number = '1'
+        self.map.actions()
 
-        while self.test != 'end':
-            answer = input("What room? 2 or 3, 4? ")
-            if answer == '2':
-                self.test = RoomTwo()
-                self.test.desc()
-            elif answer == '3':
-                self.test = RoomThree()
-                self.test.desc()
-                break
+        while self.map != 'end':
+
+            if self.map.next_action == 'OBSERVE':
+                self.map.desc()
+                self.map.actions()
+
+            elif self.map.next_action == 'SAVE':
+                pass
+
+            elif self.map.next_action == 'CHANGE ROOM':
+                answer = input("What room? 1, 2 or 3? ")
+
+                if answer == current_number:
+                    print("I am already here.")
+
+                elif answer == '1':
+                    self.map = RoomOne()
+                    current_number = '1'
+                    self.map.actions()
+
+                elif answer == '2':
+                    self.map = RoomTwo()
+                    current_number = '2'
+                    self.map.actions()
+
+                elif answer == '3':
+                    self.map = RoomThree()
+                    current_number = '3'
+                    self.map.actions()
+                    break
+
+                else:
+                    print("That room doesn't exist.")
+
+            elif self.map.next_action == 'HELP':
+                print(self.map.instructions)
+                self.map.actions()
+
             else:
-                print("That room doesn't exist.")
+                print("Error!")
+                break
 
-        if self.test.final_answer == "Life and Death" or "Life, Death" or "Life Death":
-            self.test = FinalRoom()
-            self.test.desc()
-            self.test = 'end'
+    #    if self.map.final_answer == "Life and Death" or "Life, Death" or "Life Death":
+    #        self.map = FinalRoom()
+    #        self.map.desc()
+    #        self.map = 'end'
+    #    else:
+    #        print("The room fills with fire and you burn to death.")
 
         #allow for input
         #change scene depending on input
--- rooms.py
+++ rooms.py
@@ -2,9 +2,20 @@
 # one class per room
 class Room(object):
 
+    def __init__(self):
+        self.instructions = """
+        OBSERVE describes your surroundings,
+        SAVE saves your progress. CHANGE ROOM will
+        let you move around the map. HELP will display
+        this message.
+        """
+
     def desc(self):
         self.keyword = None
         print("Enter room info here.")
+
+    def actions(self):
+        self.next_action = input("OBSERVE, SAVE, CHANGE ROOM, HELP ")
 
 class RoomOne(Room):
 
@@ -19,11 +30,12 @@ class RoomTwo(Room):
         print("Room Two Description")
 
 class RoomThree(Room):
-    final_answer = None
+
     def desc(self):
+        #self.final_answer = None
         print("Room Three Description")
-        final_answer = input("""In existance, only two things are in balance.
-        Speak truth, and pass. Speak falsehood, and be judged. """)
+        #self.final_answer = input("""In existance, only two things are in balance.
+        #Speak truth, and pass. Speak falsehood, and be judged. """)
 
 class FinalRoom(Room):
     def desc(self):
